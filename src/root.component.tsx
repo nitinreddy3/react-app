a9578ea800860c44d5da27b8238c167c9aa19d76
nitin reddy <reddynitin3@gmail.com>
2021-07-11 12:47:02 +0530

    Added a simple react component

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import App from "./App";
 
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return (
+    <section style={{ marginTop: 100 }}>
+      <App name="Testapp" />
+    </section>
+  );
 }
